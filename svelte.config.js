bd9f441dacb435d8d52ff80e41ec58a42dc9ace9
AguzzTN54 <gusagusnanang@gmail.com>
2021-09-08 13:08:33 +0700

    Add Vercel Adapter for deploying

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
+import vercel from '@sveltejs/adapter-vercel';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
