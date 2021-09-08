98d4db4934163843fd83547b1109ea4426f53475
AguzzTN54 <gusagusnanang@gmail.com>
2021-09-08 12:45:46 +0700

    Add history index page

---
+++ src/routes/history/index.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+
+	onMount(() => goto('/'));
+</script>
+
+<section>
+	<div class="box">
+		No Content available on this page, please <a href="/"> Back To The Beginning .. </a>
+	</div>
+	<div class="transparent">
+		<a href="/history/limited">limited</a>
+		<a href="/history/beginner">beginner</a>
+		<a href="/history/weapon">weapon</a>
+		<a href="/history/standard"> standard </a>
+	</div>
+</section>
+
+<style>
+	.box {
+		font-size: 1rem;
+		padding: 2rem;
+	}
+	.box a {
+		font-weight: bold;
+		color: #00aaff;
+	}
+	.transparent a {
+		color: transparent;
+		font-size: 0;
+	}
+</style>
