4b5b1dadaefdd64f16aff7a8584ae88b5ee771a9
AguzzTN54 <gusagusnanang@gmail.com>
2021-09-22 13:06:50 +0700

    Renew chance probabillity to get items

--- src/lib/setup/probability.js
+++ src/lib/setup/probability.js
@@ -2,16 +2,16 @@ const base4StarChance = [5.18, 4.88, 4.68, 5.67, 6.78, 4.6, 6.13, 12.28, 41.29, 
 const base5StarChance = [0.6];
 
 for (let i = 0; i < 4; i++) base5StarChance.push(1.11); // 2-5
-for (let i = 0; i < 5; i++) base5StarChance.push(1.44); // 6-10
-for (let i = 0; i < 5; i++) base5StarChance.push(1.47); // 11-15
-for (let i = 0; i < 5; i++) base5StarChance.push(1.42); // 16-20
-for (let i = 0; i < 5; i++) base5StarChance.push(1.6); // 21-25
-for (let i = 0; i < 5; i++) base5StarChance.push(1.57); // 26-30
-for (let i = 0; i < 10; i++) base5StarChance.push(1.51); // 31-40
+for (let i = 0; i < 5; i++) base5StarChance.push(0.44); // 6-10
+for (let i = 0; i < 5; i++) base5StarChance.push(0.47); // 11-15
+for (let i = 0; i < 5; i++) base5StarChance.push(0.42); // 16-20
+for (let i = 0; i < 5; i++) base5StarChance.push(0.6); // 21-25
+for (let i = 0; i < 5; i++) base5StarChance.push(0.57); // 26-30
+for (let i = 0; i < 10; i++) base5StarChance.push(0.51); // 31-40
 for (let i = 0; i < 10; i++) base5StarChance.push(1.57); // 41-50
-for (let i = 0; i < 10; i++) base5StarChance.push(1.31); // 51-60
+for (let i = 0; i < 10; i++) base5StarChance.push(.31); // 51-60
 for (let i = 0; i < 10; i++) base5StarChance.push(1.14); // 61-70
-for (let i = 0; i < 3; i++) base5StarChance.push(0.8); // 71-73
+for (let i = 0; i < 3; i++) base5StarChance.push(1.8); // 71-73
 base5StarChance.push(7.14); // 74
 base5StarChance.push(13.9); // 75
 base5StarChance.push(19.45); // 76
