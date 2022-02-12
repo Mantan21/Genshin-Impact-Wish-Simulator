71436aaceecdd296ebde6b1944f1d8a5a872eb43
AguzzTN54 <gusagusnanang@gmail.com>
2022-02-13 00:26:00 +0700

    Split toast to diferent files

---
+++ src/lib/components/utility/Toast.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { fly } from 'svelte/transition';
+
+	const dispatch = createEventDispatcher();
+	const timer = setTimeout(() => {
+		dispatch('close');
+		clearTimeout(timer);
+	}, 3000);
+</script>
+
+<div class="toast" transition:fly={{ y: -20 }}>
+	<slot />
+</div>
+
+<style>
+	.toast {
+		padding: 0.5rem 1.5rem;
+		text-align: center;
+		border-radius: 0.3rem;
+		min-width: 10rem;
+		max-width: 20rem;
+		color: var(--tertiary-color);
+		background-color: #4a5265;
+		position: fixed;
+		z-index: 999;
+		left: 50%;
+		top: 1rem;
+		transform: translateX(-50%);
+		font-size: 0.7rem;
+	}
+</style>
