0abe9b35b9553ea9071abc6007d264b286f59856
AguzzTN54 <gusagusnanang@gmail.com>
2022-02-14 16:53:03 +0700

    Optimize Splash Art Animation

---
+++ src/lib/components/utility/wishresult/SplashLight.svelte
@@ -0,0 +1,289 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let type = '';
+	export let rarity = 0;
+
+	onMount(() => {
+		const animate = document.querySelectorAll(`.${type}`);
+		animate.forEach((el) => {
+			el.addEventListener('animationend', () => {
+				el.classList.remove('animate');
+			});
+		});
+	});
+</script>
+
+{#if type === 'in'}
+	<div class="star{rarity} orbs in5" />
+	<div class="in animate star{rarity} orbs in1" style="animation-duration: .7s;" />
+	<div class="in animate star{rarity} orbs in2" style="animation-duration: 1.2s;" />
+	<div class="in animate star{rarity} orbs in3" style="animation-duration: 1.3s;" />
+	<div class="in animate star{rarity} orbs in4" style="animation-duration: .75s;" />
+	<img
+		src="/assets/images/utility/splatter-{rarity}star.svg"
+		alt="splatter"
+		class="animate sprite"
+	/>
+{/if}
+
+{#if type === 'out'}
+	<div
+		class="out star{rarity} orbs out1 animate"
+		style="animation-duration: 0.8s; animation-delay: 1.2s;"
+	/>
+	<div
+		class="out animate star{rarity} orbs out2"
+		style="animation-duration: 2s; animation-delay:1s;"
+	/>
+	<div
+		class="out animate star{rarity} orbs out3"
+		style="animation-duration: 1.2s; animation-delay: 1.2s;"
+	/>
+{/if}
+
+<style>
+	.sprite {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%) scale(0);
+		transform-origin: 0 0;
+		width: 140%;
+		height: auto;
+		animation-delay: 0.25s;
+	}
+
+	.sprite.animate {
+		animation: sprite forwards linear 1.3s 1;
+	}
+
+	/* Light */
+	.orbs {
+		background-color: transparent;
+		border-radius: 100%;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%) scale(0);
+		transform-origin: 0 0;
+		opacity: 0;
+	}
+	.in.animate {
+		animation: orbsIn forwards 1;
+	}
+
+	.in1 {
+		width: 300%;
+		height: 300%;
+	}
+
+	.in2 {
+		width: 180%;
+		height: 180%;
+	}
+
+	.in3 {
+		width: 80%;
+		height: 80%;
+	}
+
+	.in4 {
+		width: 120%;
+		height: 120%;
+	}
+
+	.in5 {
+		top: unset;
+		transform: translateX(-50%);
+		bottom: -40%;
+		width: 160vw;
+		height: 70%;
+		opacity: 1;
+		animation: orbs2 forwards 1s 1;
+	}
+
+	.out {
+		transform: scale(0) translate(-50%, -50%);
+		opacity: 0;
+	}
+	.out.animate {
+		animation: outOrbs forwards 1;
+	}
+
+	.out1 {
+		width: 50%;
+		height: 50%;
+		background-color: rgb(254 133 63 / 5%);
+	}
+
+	.out2 {
+		width: 100%;
+		height: 100%;
+	}
+
+	.out3 {
+		width: 150%;
+		height: 150%;
+	}
+
+	/* 5Star Color */
+	.star5.in1 {
+		background-color: rgb(254 133 63 / 5%);
+		box-shadow: 0 0 50px rgb(254 133 63 / 10%), inset 0 0 70px rgb(254 133 63 / 10%);
+	}
+	.star5.in2 {
+		box-shadow: 0 0 50px rgb(253 148 48 / 30%), inset 0 0 70px rgb(253 148 48 / 30%);
+	}
+	.star5.in3 {
+		box-shadow: 0 0 50px rgb(251 193 60 / 50%), inset 0 0 70px rgb(251 193 60 / 0%);
+		background-image: radial-gradient(rgba(251, 193, 60, 1), rgb(251, 193, 60, 0));
+	}
+	.star5.in4 {
+		box-shadow: 0 0 30px rgb(253 148 48 / 10%), inset 0 0 30px rgb(253 148 48 / 10%);
+	}
+	.star5.in5 {
+		background-image: radial-gradient(
+			rgba(253, 148, 48, 0.5),
+			rgb(253, 148, 48, 0),
+			rgba(253, 148, 48, 0)
+		);
+	}
+	.star5.out1 {
+		box-shadow: 0 0 50px rgb(251 193 60 / 50%), inset 0 0 50px rgb(251 193 60 / 50%);
+	}
+	.star5.out2 {
+		box-shadow: 0 0 150px rgb(255 255 255 / 50%), inset 0 0 170px rgb(255 255 255 / 50%);
+	}
+	.star5.out3 {
+		box-shadow: 0 0 200px rgb(253 148 48 / 60%), inset 0 0 170px rgb(253 148 48 / 60%);
+	}
+
+	/* 4 Star Color */
+	.star4.in1 {
+		background-color: rgb(156, 71, 218 / 5%);
+		box-shadow: 0 0 50px rgb(156, 71, 218 / 10%), inset 0 0 70px rgb(156, 71, 218 / 10%);
+	}
+	.star4.in2 {
+		box-shadow: 0 0 50px rgb(113 34 221 / 30%), inset 0 0 70px rgb(113 34 221 / 30%);
+	}
+	.star4.in3 {
+		box-shadow: 0 0 50px rgb(180 47 248 / 50%), inset 0 0 70px rgb(180 47 248 / 0%);
+		background-image: radial-gradient(rgba(156, 71, 218, 1), rgb(180, 47, 248, 0));
+	}
+	.star4.in4 {
+		box-shadow: 0 0 30px rgb(113 34 221 / 10%), inset 0 0 30px rgb(113 34 221 / 10%);
+	}
+	.star4.in5 {
+		background-image: radial-gradient(
+			rgba(156, 71, 218, 0.5),
+			rgb(156, 71, 218, 0),
+			rgba(156, 71, 218, 0)
+		);
+	}
+	.star4.out1 {
+		box-shadow: 0 0 50px rgb(180 47 248 / 50%), inset 0 0 50px rgb(180 47 248 / 50%);
+	}
+	.star4.out2 {
+		box-shadow: 0 0 150px rgb(255 255 255 / 50%), inset 0 0 170px rgb(255 255 255 / 50%);
+	}
+	.star4.out3 {
+		box-shadow: 0 0 200px rgb(113 34 221 / 60%), inset 0 0 170px rgb(113 34 221 / 60%);
+	}
+
+	/* 3 Star Color */
+	.star3.in1 {
+		background-color: rgb(254 133 63 / 5%);
+		box-shadow: 0 0 50px rgb(254 133 63 / 10%), inset 0 0 70px rgb(254 133 63 / 10%);
+	}
+	.star3.in2 {
+		box-shadow: 0 0 50px rgb(85 93 255 / 30%), inset 0 0 70px rgb(85 93 255 / 30%);
+	}
+	.star3.in3 {
+		box-shadow: 0 0 50px rgb(106 128 254 / 50%), inset 0 0 70px rgb(106 128 254 / 0%);
+		background-image: radial-gradient(rgba(99, 124, 205, 1), rgba(99, 124, 205, 0));
+	}
+	.star3.in4 {
+		box-shadow: 0 0 30px rgb(85 93 255 / 10%), inset 0 0 30px rgb(85 93 255 / 10%);
+	}
+	.star3.in5 {
+		background-image: radial-gradient(
+			rgba(99, 124, 205, 0.5),
+			rgba(99, 124, 205, 0),
+			rgba(99, 124, 205, 0)
+		);
+	}
+	.star3.out1 {
+		box-shadow: 0 0 50px rgb(157 217 252 / 50%), inset 0 0 50px rgb(157 217 252 / 50%);
+	}
+	.star3.out2 {
+		box-shadow: 0 0 150px rgb(255 255 255 / 50%), inset 0 0 170px rgba(99, 124, 205, 1);
+	}
+	.star3.out3 {
+		box-shadow: 0 0 200px rgb(85 93 255 / 60%), inset 0 0 170px rgb(85 93 255 / 60%);
+	}
+
+	@keyframes orbsIn {
+		0% {
+			transform: scale(0) translate(-50%, -50%);
+		}
+		90% {
+			transform: scale(1) translate(-50%, -50%);
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+
+	@keyframes orbs2 {
+		0% {
+			transform: scale(0) translateX(-50%);
+		}
+		90% {
+			transform: scale(1) translateX(-50%);
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+		}
+	}
+
+	@keyframes outOrbs {
+		30% {
+			opacity: 1;
+		}
+		100% {
+			transform: scale(3) translate(-50%, -50%);
+			opacity: 0;
+		}
+	}
+
+	@keyframes sprite {
+		0% {
+			transform: scale(0) translate(-50%, -50%);
+			opacity: 0;
+		}
+		25% {
+			transform: scale(0.75) translate(-50%, -50%);
+			opacity: 1;
+		}
+		50% {
+			transform: scale(1.5) translate(-50%, -50%);
+			opacity: 0;
+		}
+		80% {
+			transform: scale(0.6) translate(-50%, -50%);
+			opacity: 0;
+		}
+		90% {
+			transform: scale(0.8) translate(-50%, -50%);
+			opacity: 1;
+			z-index: 10;
+		}
+		100% {
+			transform: scale(1.5) translate(-50%, -50%);
+			opacity: 0;
+		}
+	}
+</style>
