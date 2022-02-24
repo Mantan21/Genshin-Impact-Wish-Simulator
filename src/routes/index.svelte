f9151f1a3c1094a0627cbacdf7fd8dbce936c5c0
AguzzTN54 <gusagusnanang@gmail.com>
2022-02-24 20:01:48 +0700

    Meta Picture

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,7 +15,7 @@
 		isLoaded
 	} from '$lib/store/stores';
 	import playSfx from '$lib/functions/audio';
-	import { APP_TITLE } from '$lib/env';
+	import { APP_TITLE, HOST } from '$lib/env';
 	import { setBannerVersionAndPhase } from '$lib/functions/importLocalData';
 	import { beginner } from '$lib/data/banners/beginner.json';
 
@@ -103,10 +103,10 @@
 	<meta name="title" content={APP_TITLE} />
 	<meta property="og:title" content={APP_TITLE} />
 	<meta property="twitter:title" content={APP_TITLE} />
-	<meta property="og:image" content="/screenshot/meta-picture.jpg" />
-	<meta name="twitter:image:src" content="/screenshot/meta-picture.jpg" />
-	<meta property="twitter:image" content="/assets/images/meta-picture.jpg" />
-	<link rel="fluid-icon" href="/screenshot/meta-picture.jpg" title={APP_TITLE} />
+	<meta property="og:image" content="{HOST}/screenshot/meta-picture.jpg" />
+	<meta name="twitter:image:src" content="{HOST}/screenshot/meta-picture.jpg" />
+	<meta property="twitter:image" content="{HOST}/assets/images/meta-picture.jpg" />
+	<link rel="fluid-icon" href="{HOST}/screenshot/meta-picture.jpg" title={APP_TITLE} />
 </svelte:head>
 
 <Loader />
