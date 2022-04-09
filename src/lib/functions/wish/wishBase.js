a0776349b85f8a468b6fb66e9cd11eadd26be1e4
AguzzTN54 <gusagusnanang@gmail.com>
2022-04-09 18:09:21 +0700

    Split Wish Function

---
+++ src/lib/functions/wish/wishBase.js
@@ -0,0 +1,75 @@
+import weaponsDB from '$lib/data/weapons.json';
+import charsDB from '$lib/data/characters.json';
+
+const rand = (array) => array[Math.floor(Math.random() * array.length)];
+
+const getAllChars = (star) =>
+	charsDB.data
+		.find(({ rarity }) => rarity === star)
+		.list.map((arr) => {
+			arr.type = 'character';
+			arr.rarity = star;
+			return arr;
+		});
+
+const getAllWeapons = (star) =>
+	weaponsDB.data
+		.find(({ rarity }) => rarity === star)
+		.list.map((arr) => {
+			arr.type = 'weapon';
+			arr.rarity = star;
+			return arr;
+		});
+
+const standardWeapons = (star) => getAllWeapons(star).filter(({ limited }) => !limited);
+const standardChars5Star = (exclude) => getAllChars(5).filter(({ name }) => exclude.includes(name));
+
+const get4StarChars = getAllChars(4).filter(({ name }) => {
+	return !charsDB.onlyStandard.includes(name);
+});
+
+const filterCharByReleased = (charlist, version = null, phase = null) => {
+	return charlist.filter(({ release }) => {
+		if (!release) return true;
+		const [v, phs] = release.split('-');
+		if (parseFloat(version) < parseFloat(v)) return false;
+		if (parseFloat(version) === parseFloat(v) && phase <= parseInt(phs)) return false;
+		return true;
+	});
+};
+
+const get3StarItem = () => rand(standardWeapons(3));
+const get4StarItem = (bannerToRoll = 'allExcludeStandard', version = null, phase = null) => {
+	const itemType = rand(['weap', 'char']);
+
+	// show standard character exclude starter character ( amber, kaeya, lisa )
+	let charList = get4StarChars;
+
+	// Show All standard chars exlude new character
+	if (bannerToRoll === 'standard') charList = getAllChars(4);
+
+	// show standard item exclude starter and new character
+	if (bannerToRoll === 'limited') charList = get4StarChars;
+
+	const items = itemType === 'weap' ? standardWeapons(4) : charList;
+	const filtered = filterCharByReleased(items, version, phase);
+	return rand(filtered);
+};
+
+const getStandard5StarItem = ({ exclude }) => {
+	const itemType = rand(['weap', 'char']);
+	const items = itemType === 'weap' ? standardWeapons(5) : standardChars5Star(exclude);
+	return rand(items);
+};
+
+export {
+	rand,
+	getAllChars,
+	getAllWeapons,
+	get3StarItem,
+	get4StarItem,
+	get4StarChars,
+	getStandard5StarItem,
+	standardWeapons,
+	standardChars5Star
+};
---
+++ src/lib/functions/wish/wishLimited.js
@@ -0,0 +1,57 @@
+import { getAllChars, get3StarItem, get4StarItem, standardChars5Star, rand } from './wishBase';
+import { nextGuaranteed } from '$lib/store/localstore';
+
+const limitedWish = {
+	init(eventsData, indexOfBanner) {
+		const { item, rateup } = eventsData;
+		this._character = item;
+		this._rateup = rateup;
+		this._indexOfBanner = indexOfBanner;
+		return this;
+	},
+
+	_rateupChars() {
+		return getAllChars(4).filter(({ name }) => this._rateup.includes(name));
+	},
+
+	_featuredChars() {
+		let { _character, _indexOfBanner } = this;
+		if (Array.isArray(_character)) _character = _character[_indexOfBanner];
+		const result = getAllChars(5).find(({ name }) => name === _character.character);
+		return result;
+	},
+
+	get(rarity, opt) {
+		const { version, phase, excluded } = opt;
+		if (rarity === 3) return get3StarItem();
+		if (rarity === 4) {
+			const resultType = rand(['rateup', 'std']);
+			if (resultType === 'std') return get4StarItem('limited', version, phase);
+
+			// If rate up character
+			return rand(this._rateupChars());
+		}
+
+		if (rarity === 5) {
+			// Guaranteed
+			const limitedResult = this._featuredChars();
+			if (nextGuaranteed.get() === 'yes') {
+				nextGuaranteed.set('no');
+				return limitedResult;
+			}
+
+			// not guaranteed
+			const resultType = rand(['limited', 'std']);
+			if (resultType === 'std') {
+				nextGuaranteed.set('yes');
+				return rand(standardChars5Star(excluded));
+			}
+
+			// win 50: 50
+			nextGuaranteed.set('no');
+			return limitedResult;
+		}
+	}
+};
+
+export default limitedWish;
