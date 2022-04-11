523f87db94750dba5f25f94eca1846a1998e4ea3
AguzzTN54 <gusagusnanang@gmail.com>
2022-04-11 20:21:11 +0700

    Iklan

--- src/lib/components/utility/Iklan.svelte
+++ src/lib/components/utility/Iklan.svelte
@@ -3,30 +3,23 @@
 
 	export let type = '';
 
-	const rand = (array) => array[Math.floor(Math.random() * array.length)];
-	const ad = rand([1, 2]);
+	// const rand = (array) => array[Math.floor(Math.random() * array.length)];
+	// const ad = rand([1, 2]);
 </script>
 
 <svelte:head>
 	{#if type === 'banner'}
-		{#if ad === 1}
-			<amp-ad
-				width="100vw"
-				height="320"
-				type="adsense"
-				data-ad-client="ca-pub-1874822310102113"
-				data-ad-slot="6827309798"
-				data-auto-format="rspv"
-				data-full-width=""
-			>
-				<div overflow="" />
-			</amp-ad>
-		{:else}
-			<script
-				data-cfasync="false"
-				type="text/javascript"
-				src="//onclickprediction.com/a/display.php?r=5729138"></script>
-		{/if}
+		<amp-ad
+			width="100vw"
+			height="320"
+			type="adsense"
+			data-ad-client="ca-pub-1874822310102113"
+			data-ad-slot="6827309798"
+			data-auto-format="rspv"
+			data-full-width=""
+		>
+			<div overflow="" />
+		</amp-ad>
 	{:else}
 		{#if !$isMobile}
 			<script
@@ -35,6 +28,11 @@
 				data-adel="atag"
 				src="//acacdn.com/script/atg.js"
 				czid="v1xd6wvvpe"></script>
+
+			<!-- <script
+				data-cfasync="false"
+				type="text/javascript"
+				src="//onclickprediction.com/a/display.php?r=5729138"></script> -->
 		{:else}
 			<script
 				data-cfasync="false"
