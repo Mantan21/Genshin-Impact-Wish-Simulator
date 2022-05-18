cd7e8c4bac06be165d77c429e0782953db119485
AguzzTN54 <gusagusnanang@gmail.com>
2022-05-18 11:13:23 +0700

    Server Migration

--- src/lib/components/utility/Iklan.svelte
+++ src/lib/components/utility/Iklan.svelte
@@ -1,40 +1,32 @@
 <script>
 	import { onMount } from 'svelte';
-	import { isMobile } from '$lib/store/stores';
 
 	export let type = '';
 	export let head = false;
 
-	const rand = (array) => array[Math.floor(Math.random() * array.length)];
-	const ad = rand([1, 2]);
-
 	onMount(() => {
 		if (type !== 'banner') return;
-		return (window.adsbygoogle = window.adsbygoogle || []).push({});
+		(window.adsbygoogle = window.adsbygoogle || []).push({});
+		return;
 	});
 </script>
 
 {#if type === 'banner'}
 	<ins
 		class="adsbygoogle"
-		style={{ display: 'inline-block', width: '728px', height: '90px' }}
+		style="display:block"
 		data-ad-client="ca-pub-1874822310102113"
-		data-ad-slot="22721704232"
+		data-ad-slot="6827309798"
+		data-ad-format="auto"
+		data-full-width-responsive="true"
 	/>
 {/if}
 
 {#if head}
-	{#if !$isMobile}
-		<script
-			data-cfasync="false"
-			type="text/javascript"
-			data-adel="atag"
-			src="//acdcdn.com/script/atg.js"
-			czid="nmai2xdfj8"></script>
-	{/if}
-
-	{#if ad === 1 || $isMobile}
-		<script type="text/javascript" src="https://wap4dollar.com/ad/pops/?id=jnf1abpn5a"></script>
-	{/if}
-	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script
+		data-cfasync="false"
+		type="text/javascript"
+		data-adel="atag"
+		src="//acacdn.com/script/atg.js"
+		czid="v1xd6wvvpe"></script>
 {/if}
