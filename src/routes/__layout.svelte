9d64e013982464a2eb7be450eef07cd395766d1e
AguzzTN54 <gusagusnanang@gmail.com>
2022-07-08 06:23:28 +0700

    New Feature: Blessing of the Welkin Moon are available to purchase

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
 	// Packagae
 	import { page } from '$app/stores';
 	import { dev } from '$app/env';
-	import { onMount, setContext } from 'svelte';
+	import { onMount } from 'svelte';
 
 	import {
 		viewportHeight,
@@ -16,14 +16,12 @@
 		mobileMode,
 		isAcquaintUsed,
 		bannerActive,
-		bannerList,
-		backsound
+		bannerList
 	} from '$lib/store/stores';
 	import { HOST, DESCRIPTION, KEYWORDS } from '$lib/env';
 	import { importLocalBalance } from '$lib/functions/importLocalData';
 	import { mobileDetect } from '$lib/functions/mobileDetect';
 	import Ads from '$lib/components/utility/Iklan.svelte';
-	import Disclaimer from '$lib/components/utility/Disclaimer.svelte';
 	import '../app.css';
 
 	$: preview = $page.url.pathname.split('/')[1] === 'screen';
@@ -38,14 +36,6 @@
 		const rotate = angle === 90 || angle === 270;
 		mobileMode.set(rotate);
 	};
-
-	let showDisclaimer = true;
-	const closeDisclaimer = () => {
-		backsound.set(true);
-		bannerActive.set(0);
-		showDisclaimer = false;
-	};
-	setContext('closeDisclaimer', closeDisclaimer);
 
 	onMount(() => {
 		importLocalBalance();
@@ -80,10 +70,6 @@
 	<meta property="al:web:url" content={HOST} />
 	<Ads head />
 </svelte:head>
-
-{#if !preview}
-	<Disclaimer show={showDisclaimer} />
-{/if}
 
 <main
 	class:mobile={$mobileMode}
