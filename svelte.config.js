3e00de27217a2d25d2db8282d3fadc0091c642c4
AguzzTN54 <gusagusnanang@gmail.com>
2022-07-25 07:24:17 +0700

    Config Image Preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,30 @@ const { NODE_ENV, USE_CDN, GITHUB_USER, GITHUB_REPO } = process.env;
 const cdn_on = NODE_ENV === 'production' && USE_CDN === 'true';
 const cdn_url = `https://cdn.jsdelivr.net/gh/${GITHUB_USER}/${GITHUB_REPO}/static/assets/`;
 
-const imagePreprocess = () => image({});
+const imagePreprocess = () => {
+	return image({
+		processFolders: ['images/banner'],
+		processFoldersRecursively: true,
+		processFoldersSizes: true,
+		processFoldersExtensions: ['webp'],
+		outputDir: './g',
+		publicDir: './static/',
+		placeholder: 'blur',
+		sizes: [400, 800],
+		breakpoints: [375, 768],
+		webp: true,
+		webpOptions: {
+			quality: 75,
+			lossless: false,
+			force: true
+		}
+		// trace: {
+		// 	background: '#fff',
+		// 	color: '#002fa7',
+		// 	threshold: 120
+		// }
+	});
+};
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
