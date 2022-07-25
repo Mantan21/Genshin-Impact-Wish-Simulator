f527fd4086f68532c9502f9a5a883a67f44815fb
AguzzTN54 <gusagusnanang@gmail.com>
2022-07-25 08:07:13 +0700

    Fix: Register only available Locale

--- src/lib/functions/i18n.js
+++ src/lib/functions/i18n.js
@@ -2,14 +2,17 @@ import { browser } from '$app/env';
 import { init, register, getLocaleFromNavigator } from 'svelte-i18n';
 
 const supportedLocales = ['en-US', 'id-ID', 'zh-CN'];
+const itemLocales = ['en-US', 'zh-CN'];
 
 const mountLocale = () => {
 	supportedLocales.forEach((langID) => {
 		register(langID, () => import(`../../locales/${langID}.json`));
+	});
+
+	itemLocales.forEach((langID) => {
 		register(langID, () => import(`../../locales/characters/${langID}.json`));
 		register(langID, () => import(`../../locales/weapons/${langID}.json`));
 	});
-	
 
 	const savedLocale = browser ? localStorage.getItem('locale') : 'en';
 	const isLocale = savedLocale && supportedLocales.includes(savedLocale);
