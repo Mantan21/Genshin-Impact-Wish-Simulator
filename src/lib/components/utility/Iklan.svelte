052ae1e9263f8378c89842f604b31169e4480b32
AguzzTN54 <gusagusnanang@gmail.com>
2022-08-04 01:14:03 +0700

    Disable Google Adsense

--- src/lib/components/utility/Iklan.svelte
+++ src/lib/components/utility/Iklan.svelte
@@ -1,27 +1,27 @@
 <script>
 	import { dev } from '$app/env';
-	import { onMount } from 'svelte';
+	// import { onMount } from 'svelte';
 	import { isMobile } from '$lib/store/stores';
 
 	export let type = '';
 	export let head = false;
 
-	onMount(() => {
-		if (type !== 'banner') return;
-		(window.adsbygoogle = window.adsbygoogle || []).push({});
-		return;
-	});
+	// onMount(() => {
+	// 	if (type !== 'banner') return;
+	// 	(window.adsbygoogle = window.adsbygoogle || []).push({});
+	// 	return;
+	// });
 </script>
 
 {#if type === 'banner'}
-	<ins
+	<!-- <ins
 		class="adsbygoogle"
 		style="display:block"
 		data-ad-client="ca-pub-1874822310102113"
 		data-ad-slot="6827309798"
 		data-ad-format="auto"
 		data-full-width-responsive="true"
-	/>
+	/> -->
 {/if}
 
 {#if head && !dev}
