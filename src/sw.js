31b43c6b8c48c3956d2803504d6d9e09216e3cbc
AguzzTN54 <gusagusnanang@gmail.com>
2024-12-30 17:17:35 +0700

    Service Worker & Icon Update

--- src/sw.js
+++ src/sw.js
@@ -3,8 +3,11 @@ import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { CacheFirst, NetworkFirst } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration/ExpirationPlugin';
 
-const cacheVersion = 'v1';
+const cacheVersion = 'v2';
+const maxAgeSeconds = 15 * 24 * 60 * 60; // 15 Days
+const maxEntries = 60;
 
 clientsClaim();
 skipWaiting();
@@ -20,13 +23,20 @@ let precache = self.__WB_MANIFEST;
 precache = [];
 precacheAndRoute(precache, { ignoreURLParametersMatching: [/.*/] });
 
-registerRoute('/', new NetworkFirst({ cacheName: `Static-${cacheVersion}` }));
+registerRoute(
+	'/',
+	new NetworkFirst({
+		cacheName: `Static-${cacheVersion}`,
+		plugins: [new ExpirationPlugin({ maxEntries, maxAgeSeconds })]
+	})
+);
 
 registerRoute(
 	new RegExp('.(?:/?pwa=true|/?pwasc)'),
 	new NetworkFirst({
 		cacheName: `Static-${cacheVersion}`,
 		plugins: [
+			new ExpirationPlugin({ maxEntries, maxAgeSeconds }),
 			{
 				cachedResponseWillBeUsed: ({ cachedResponse }) => {
 					if (cachedResponse) return cachedResponse;
@@ -44,19 +54,17 @@ registerRoute(
 		const isVideo = url.href.includes('/videos');
 
 		const imgPath = url.pathname.includes('/internal/immutable/assets');
-		const iconPath = url.pathname.includes('/icons');
 		const transformPath = url.href.match(/(\/transform\/|\/cb\/)/);
-		const imagePaths = imgPath || iconPath || transformPath;
+		const imagePaths = imgPath || transformPath;
 
 		const matchImage = url.href.match(new RegExp('.(?:svg|webp|jpg|png|jpeg)')) || [];
 		const isMatch = matchImage.length > 0;
-
 		const isImage = (request.destination === 'image' || isMatch) && imagePaths;
-
 		return isAudio || isImage || isVideo;
 	},
 	new CacheFirst({
-		cacheName: `Static-${cacheVersion}`
+		cacheName: `Static-${cacheVersion}`,
+		plugins: [new ExpirationPlugin({ maxEntries, maxAgeSeconds })]
 	})
 );
 
@@ -67,21 +75,33 @@ registerRoute(
 	},
 	new NetworkFirst({
 		cacheName: `Static-${cacheVersion}`,
-		plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
+		plugins: [
+			new CacheableResponsePlugin({ statuses: [0, 200] }),
+			new ExpirationPlugin({ maxEntries, maxAgeSeconds })
+		]
 	})
 );
 
 registerRoute(
-	new RegExp('.(?:css|js|json)$'),
-	new NetworkFirst({
-		cacheName: 'Chunks'
+	new RegExp('/internal/immutable/assets'),
+	new CacheFirst({
+		cacheName: `Chunks-${cacheVersion}`,
+		plugins: [new ExpirationPlugin({ maxEntries, maxAgeSeconds })]
 	})
 );
 
 registerRoute(
-	({ url }) => url.href.match('/js/image-cdn'),
+	({ url }) => {
+		const cdnParser = url.href.match('/js/image-cdn');
+		const isIcons = url.pathname.match('/icons');
+		const isStatic = url.pathname.match(new RegExp('.(?:css|js|json)$'));
+		return cdnParser || isIcons || isStatic;
+	},
 	new NetworkFirst({
-		cacheName: 'Chunks',
-		plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
+		cacheName: `Chunks-${cacheVersion}`,
+		plugins: [
+			new CacheableResponsePlugin({ statuses: [0, 200] }),
+			new ExpirationPlugin({ maxEntries, maxAgeSeconds })
+		]
 	})
 );
