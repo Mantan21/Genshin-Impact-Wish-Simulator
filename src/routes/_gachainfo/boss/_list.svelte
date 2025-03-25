1a9f29556201111e0dec861851d28757a9bf0547
Daine-C <danielleverna.caasi@g.msuiit.edu.ph>
2025-03-25 14:27:33 +0800

    Working logout

--- src/routes/_gachainfo/boss/_list.svelte
+++ src/routes/_gachainfo/boss/_list.svelte
@@ -26,6 +26,21 @@
 		await axios.post("http://localhost:3001/api/boss", { bossStatus }, { withCredentials: true });
 	}
 
+	let banner;
+
+	let processedUpdates = [...updates.data].reverse();
+
+	for(let uppy of processedUpdates){
+		console.log("Checking update:", uppy.patch, "against version:", version);
+
+    	if (Number(uppy.patch) === Number(version)) {
+			console.log(uppy.banner)
+			banner = uppy.banner;
+        	console.log("Match found! Banner set to:", banner);
+        	break;
+    	}
+	}
+
 	function healthier(){ //HP Scaling
 		let mult = Number(version);
 
@@ -70,6 +85,12 @@
 	}
 
 		frame();
+
+		let boss = storageLocal.get('boss')
+		if (!boss) {
+    		boss = {};
+		}
+		storageLocal.set('boss', boss);
 	};
 
 	async function dealDamage() {
@@ -92,6 +113,14 @@
 
 		console.log("health:",health);
 		healthBar.updateHealth(health);
+
+		let boss = storageLocal.get('boss')
+		for(let ban of banner){
+			boss[ban] = bossDefeated;
+			console.log(boss, ban, boss[ban])
+			storageLocal.set('boss', boss);
+		}
+		console.log("After setting boss:", storageLocal.get("boss"));
 	}
 
 	onMount(setupCanvas);
